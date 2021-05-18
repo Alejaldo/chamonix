0c3c375daec9e7865b250d126166f95e760757f7
Alejaldo <statfluct@yahoo.com>
2021-05-18 22:03:08 +0500

    Create all about users and refs with events

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  VALID_EMAIL = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
+  has_many :events
+
+  validates :name, presence: true, length: {maximum: 35}
+  validates :email, presence: true, length: {maximum: 255}, uniqueness: true, format: { with: VALID_EMAIL }
+end
