6b272658975df9a6d974aa80beed97b2d38f9244
Alejaldo <statfluct@yahoo.com>
2021-05-21 01:48:47 +0500

    Incept webpack 3 step

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -5,17 +5,18 @@
 
 import Rails from "@rails/ujs"
 // import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-//import "channels"
+// import * as ActiveStorage from "@rails/activestorage"
+// import "channels"
+
+Rails.start()
+// Turbolinks.start()
+// ActiveStorage.start()
+
+// require("@rails/ujs").start()
 
 import 'bootstrap/dist/js/bootstrap'
 import '../styles/application'
 import '../styles/events'
 import '../styles/registration'
-
-
-Rails.start()
-// Turbolinks.start()
-ActiveStorage.start()
 
 const images = require.context('../images', true)
