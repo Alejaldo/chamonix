00b31fd58b299807b7d37b93e9cf3450ca122700
Alejaldo <statfluct@yahoo.com>
2021-05-25 11:26:54 +0500

    Create model, controller, db and views for subscriptions

---
+++ app/models/subscription.rb
@@ -0,0 +1,27 @@
+class Subscription < ApplicationRecord
+  VALID_EMAIL = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
+  belongs_to :event
+  belongs_to :user, optional: true
+
+  validates :user, uniqueness: {scope: :event_id}, if: -> { user.present? }
+  validates :user_name, presence: true, unless: -> { user.present? }
+  validates :user_email, presence: true, format: { with: VALID_EMAIL }, unless: -> { user.present? }
+  validates :user_email, uniqueness: {scope: :event_id}, unless: -> { user.present? }
+
+  def user_name
+    if user.present?
+     user.name
+    else
+     super
+    end
+  end
+
+  def user_email
+    if user.present?
+      user.email
+    else
+      super
+    end
+  end
+end
