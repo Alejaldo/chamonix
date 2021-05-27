586817f747e1f713f2d03ce2f5c7d231dac325fb
Alejaldo <statfluct@yahoo.com>
2021-05-27 10:56:35 +0500

    Delete word_translate method from all files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  helper_method :current_user_can_edit?, :words_translate
+  helper_method :current_user_can_edit?
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:account_update, keys: [:password, :password_confirmation, :current_password])
@@ -9,9 +9,5 @@ class ApplicationController < ActionController::Base
 
   def current_user_can_edit?(model)
     user_signed_in? && (model.user == current_user || (model.try(:event).present? && model.event.user == current_user))
-  end
-
-  def words_translate(words)
-    I18n.t "#{words}"
   end
 end
