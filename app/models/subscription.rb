056b89ce81f6b893ea7883c327d1cbcaca5bc531
Alejaldo <statfluct@yahoo.com>
2021-05-29 23:36:08 +0500

    Add registrated user email validation to Subscribtion model

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -4,10 +4,11 @@ class Subscription < ApplicationRecord
   belongs_to :event
   belongs_to :user, optional: true
 
-  validates :user, uniqueness: {scope: :event_id}, if: -> { user.present? }
+  validates :user, uniqueness: { scope: :event_id }, if: -> { user.present? }
   validates :user_name, presence: true, unless: -> { user.present? }
   validates :user_email, presence: true, format: { with: VALID_EMAIL }, unless: -> { user.present? }
-  validates :user_email, uniqueness: {scope: :event_id}, unless: -> { user.present? }
+  validates :user_email, uniqueness: { scope: :event_id }, unless: -> { user.present? }
+  validate :registrated_user_email
 
   def user_name
     if user.present?
@@ -22,6 +23,14 @@ class Subscription < ApplicationRecord
       user.email
     else
       super
+    end
+  end
+
+  private
+
+  def registrated_user_email
+    if User.exists?(email: user_email)
+      errors.add(:email, I18n.t('titles.registrated_user_email'))
     end
   end
 end
