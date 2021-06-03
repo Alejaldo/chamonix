eb7b40cbaf4ebca48bcc67783ec5eb70784f074e
Alejaldo <statfluct@yahoo.com>
2021-06-03 09:57:52 +0500

    Update Photo and Event models with photo associations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,10 @@
 class Event < ApplicationRecord
   belongs_to :user
+
   has_many :comments, dependent: :destroy
   has_many :subscriptions, dependent: :destroy
   has_many :subscribers, through: :subscriptions, source: :user
+  has_many :photos
 
   validates :title, presence: true, length: {maximum: 255}
   validates :address, presence: true
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,6 @@
 class Photo < ApplicationRecord
   belongs_to :event
   belongs_to :user
+
+  mount_uploader :photo, PhotoUploader
 end
