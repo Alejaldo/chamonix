7b3fdc18df25ff7f08da1ec0fdd175d6078a05de
Alejaldo <statfluct@yahoo.com>
2021-06-08 18:31:36 +0500

    Create EventMailer class with comment, subs and photo methods and views

---
+++ app/mailers/event_mailer.rb
@@ -0,0 +1,23 @@
+class EventMailer < ApplicationMailer
+  def subscription(event, subscription)
+    @email = subscription.user_email
+    @name = subscription.user_name
+    @event = event
+
+    mail to: event.user.email, subject: "Новая подписка на #{event.title}"
+  end
+
+  def comment(event, comment, email)
+    @comment = comment
+    @event = event
+
+    mail to: email, subject: "Новый комментарий @ #{event.title}"
+  end
+
+  def photo(event, photo, email)
+    @photo = photo
+    @event = event
+
+    mail to: email, subject: "Новая фотография @ #{event.title}"
+  end
+end
