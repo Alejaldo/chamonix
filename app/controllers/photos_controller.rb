3feb86c805ca6493c4de34bb3973341019cd2233
Alejaldo <statfluct@yahoo.com>
2021-06-09 12:30:17 +0500

    Add email notofication about new photos to PhotosController

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -7,6 +7,7 @@ class PhotosController < ApplicationController
     @new_photo.user = current_user
 
     if @new_photo.save
+      notify_about_photo(@event, @new_photo)
       redirect_to @event, notice: I18n.t('controllers.photos.created')
     else
       render 'events/show', alert: I18n.t('controllers.photos.error')
@@ -38,4 +39,11 @@ class PhotosController < ApplicationController
   def photo_params
     params.fetch(:photo, {}).permit(:photo)
   end
+
+  def notify_about_photo(event, photo)
+    all_emails = (event.subscriptions.map(&:user_email) + [event.user.email] - [photo.user.email]).uniq
+
+    all_emails.each do |mail|
+      EventMailer.comment(event, comment, mail).deliver_now
+    end
 end
