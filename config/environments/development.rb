5d2883145cfebfcfee62464006c2976698adcc24
Alejaldo <statfluct@yahoo.com>
2021-06-12 13:24:42 +0500

    Insert letter_opener gem for development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,18 +40,9 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
-  config.action_mailer.delivery_method = :mailjet
+  config.action_mailer.delivery_method = :letter_opener
 
-  #config.action_mailer.perform_deliveries = true
-
-  config.action_mailer.smtp_settings = {
-    address: 'in-v3.mailjet.com',
-    port: '587',
-    user_name: ENV["MAILJET_API_KEY"],
-    password: ENV["MAILJET_SECRET_KEY"],
-    authentication: 'plain',
-    enable_starttls_auto: true
-  }
+  config.action_mailer.perform_deliveries = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
