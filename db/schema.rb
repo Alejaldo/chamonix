aced3a41cc23f94ef3ddc7d56fd99b752c98f7a0
Alejaldo <statfluct@yahoo.com>
2021-06-13 20:45:16 +0500

    Create AddPincodeToEvents migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_03_044624) do
+ActiveRecord::Schema.define(version: 2021_06_13_153954) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2021_06_03_044624) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+    t.string "pincode"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
