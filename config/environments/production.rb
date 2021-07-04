827cab9fd9a419b9de2aa2a526705dd0a7df2395
Alejaldo <statfluct@yahoo.com>
2021-07-04 22:32:20 +0500

    Change config.action_mailer.default_url_options from Heroku server to DigOc. vps

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: 'chamonix.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: '206.189.102.132' }
   # config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :mailjet
 
