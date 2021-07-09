fd1ce0b4f08c073d3dc4f99002849e40b7eb177d
Alejaldo <statfluct@yahoo.com>
2021-07-09 14:12:51 +0500

    Change from webpack to frontend

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,6 +40,6 @@ append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bund
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
 
-set :assets_roles, :webpack
+set :assets_roles, :frontend
 set :assets_prefix, 'packs'
 # set :assets_manifests, lambda { [release_path.join('public', fetch(:assets_prefix), 'manifest.json*')] }
